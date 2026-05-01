1a4384372f8239f1297b46ee2aea583bb2188995
sopnilali <sopnilali24@gmail.com>
2026-05-01 12:39:22 +0600

    update server

--- prisma/migrations/20250506163335_add_payment_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `originalAmount` to the `payments` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "payments" ADD COLUMN     "discountPercentage" DOUBLE PRECISION NOT NULL DEFAULT 0,
-ADD COLUMN     "originalAmount" DOUBLE PRECISION NOT NULL;
